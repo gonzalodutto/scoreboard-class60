2420b5cf1c1dc7211d30cbf6e3c1b95622ff4ba3
gonzalodutto <gonzalodutto@gmail.com>
2022-07-06 13:47:45 +0200

    increment score using callback props

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,8 +1,14 @@
 export default function Player(props) {
+  const onClickIncrement = () => {
+    // call the callback prop passed down from the scoreboard
+    props.incrementScore(props.id);
+  };
+
   return (
     <li className="Player">
       <p>
         {props.name} (score: {props.score})
+        <button onClick={onClickIncrement}>increment</button>
       </p>
     </li>
   );
